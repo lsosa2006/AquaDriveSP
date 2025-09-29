6bb16a040447a81cdbf19e24f036d75d9319f0c7
lsosa2006 <luis.sosa520@pascualbravo.edu.co>
2025-09-29 16:14:23 -0500

    Se crea metodo para reducir el delay en la db

--- AquaDriveSP/Global.asax.cs
+++ AquaDriveSP/Global.asax.cs
@@ -1,6 +1,8 @@
+using AquaDriveSP.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,23 @@ namespace AquaDriveSP
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Task.Run(() => CalentarDb());
+        }
+
+        private void CalentarDb()
+        {
+            try
+            {
+                using (var db = new AppDbContext())
+                {
+                    // Ejecutamos un query ligero para inicializar la conexión
+                    db.Database.SqlQuery<int>("SELECT 1;").FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error al calentar la DB: " + ex.Message);
+            }
         }
     }
 }
